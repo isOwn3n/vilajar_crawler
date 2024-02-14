dcc74fa029617ac8334a49c35446ddc4cc7fb7c0
ownen <mr.joker.mr.joke@gmail.com>
2024-02-14 18:17:42 +0330

    add two new functions, get_from_db and put_in_json then get some new data from the site. first vilaname and then city of them

---
+++ README.md
@@ -0,0 +1,7 @@
+### if you want put it in json file add blow code in end of your code
+```python
+put_in_json(get_from_db())
+```
+
+> [!NOTE]
+> The result.json file is a sample
--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import sqlite3
+import json
 from time import sleep
+
+# from time import sleep
 import requests
 from bs4 import BeautifulSoup
 
 
-
 URL = "https://www.vilajar.com"
+
 
 def get_users_url() -> list[str]:
     # page = open('index.html', 'r')
@@ -20,16 +23,19 @@ def get_users_url() -> list[str]:
 
 
 def get_phone(user_page) -> tuple[str]:
-    print(user_page)
     req = requests.Session()
     page = req.get(user_page).content.decode()
     soup2 = BeautifulSoup(page, "html.parser")
     span = soup2.find("span", class_="ms-2 d-flex")
-    print(span)
     phone_number = span.get_text(strip=True).replace("موبایل", "")
     name = soup2.find("h4", class_="my-1 fs-6").get_text()
+    address = (
+        soup2.find("h1", class_="d-none d-sm-flex fs-1 mb-0 pb-0").find("a").get_text()
+    )
+    city = soup2.find("a", class_="city").get_text()
+    print(city)
 
-    return (name, phone_number)
+    return (name, phone_number, address, city)
 
 
 def put_in_db(data) -> None:
@@ -40,13 +46,38 @@ def put_in_db(data) -> None:
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT NOT NULL,
-            phone_number TEXT NOT NULL
+            phone_number TEXT NOT NULL,
+            address TEXT NOT NULL,
+            city TEXT NOT NULL
         )
         """
     )
     conn.commit()
-    cur.execute("insert into users (username, phone_number) values(?, ?)", data)
+    cur.execute(
+        "insert into users (username, phone_number, address, city) values(?, ?, ?, ?)",
+        data,
+    )
     conn.commit()
+
+
+def get_from_db() -> None:
+    conn = sqlite3.connect("db.sqlite3")
+    cur = conn.cursor()
+    return cur.execute("select * from users").fetchall()
+
+
+def put_in_json(data):
+    _all = [
+        {
+            "name": name[1].strip("\r"),
+            "phone": name[2],
+            "villa": name[3],
+            "city": name[4],
+        }
+        for name in data
+    ]
+    f = open("result.json", "w")
+    f.write(str(_all))
 
 
 main_page = get_users_url()
