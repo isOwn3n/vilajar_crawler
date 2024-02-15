d984c3ad9e30647713317a81796d0724a482ec0e
ownen <mr.joker.mr.joke@gmail.com>
2024-02-15 12:31:29 +0330

    some changes

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 import sqlite3
 import json
 from time import sleep
+import fake_useragent
+import random
 
-# from time import sleep
 import requests
 from bs4 import BeautifulSoup
 
-
+URL2 = "https://www.vilajar.com/RentVilla/1/price-30000-2400000,home-0-3,sort-date-desc"
 URL = "https://www.vilajar.com"
 
 
+def load_proxy():
+    f = open("proxy.json")
+    proxy = json.loads(f.read())
+    x = []
+    for i in proxy:
+        x.append({"http": f"socks4://{i['ip']}:{i['port']}"})
+    return x
+
+
+proxy = load_proxy()
+print(proxy[0])
+# random.choice(proxy)
+
+
 def get_users_url() -> list[str]:
-    # page = open('index.html', 'r')
-    page = requests.get(URL).content.decode()
+    page = requests.get(URL2).content.decode()
     soup = BeautifulSoup(page, "html.parser")
     villaBox = soup.find(id="villaBox")
     villa_item = villaBox.find_all("figure", class_="villa-item")
@@ -23,8 +37,10 @@ def get_users_url() -> list[str]:
 
 
 def get_phone(user_page) -> tuple[str]:
+    header = {"User-Agent": fake_useragent.UserAgent().random}
     req = requests.Session()
-    page = req.get(user_page).content.decode()
+    print(dict(http=f'socks4://{random.choice(proxy)}'))
+    page = req.get(user_page, headers=header, proxies=random.choice(proxy)).content.decode()
     soup2 = BeautifulSoup(page, "html.parser")
     span = soup2.find("span", class_="ms-2 d-flex")
     phone_number = span.get_text(strip=True).replace("موبایل", "")
@@ -33,8 +49,6 @@ def get_phone(user_page) -> tuple[str]:
         soup2.find("h1", class_="d-none d-sm-flex fs-1 mb-0 pb-0").find("a").get_text()
     )
     city = soup2.find("a", class_="city").get_text()
-    print(city)
-
     return (name, phone_number, address, city)
 
 
@@ -80,10 +94,17 @@ def put_in_json(data):
     f.write(str(_all))
 
 
+class TooManyRequests(Exception):
+    """Too Many Requests"""
+
+
 main_page = get_users_url()
 
 for k, i in enumerate(main_page):
-    sleep(15)
-    print(k)
-    data = get_phone(URL + i)
-    put_in_db(data)
+    try:
+        print(k + 1)
+        data = get_phone(URL + i)
+        put_in_db(data)
+    except AttributeError:
+        print("Error")
+        sleep(60 * 6)
